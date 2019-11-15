1f406d0342e5bca024e807168a7da0acdcd412a8
Dom <domeccleston@gmail.com>
2019-11-15 08:53:01 +0000

    login route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 const Users = require("./authModel");
+const bcrypt = require('bcryptjs');
 
 router.post("/register", (req, res) => {
   let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 11);
 
-  Users.addUser(user)
+  const newUser = {
+    username: req.body.username,
+    password: hash,
+  };
+
+  Users.addUser(newUser)
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -14,7 +21,20 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({ message: `Welcome ${user.username}!`});
+    } else {
+      res.status(401).json({ message: `Invalid Credentials`});
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
 });
 
 module.exports = router;
--- auth/authModel.js
+++ auth/authModel.js
@@ -4,6 +4,10 @@ function findById(id) {
     return db('users').where({ id }).first();
 }
 
+function findBy(parameter) {
+    return db('users').where(parameter);
+}
+
 async function addUser(user) {
     const [id] = await db('users').insert(user);
     return findById(id);
@@ -12,4 +16,5 @@ async function addUser(user) {
 module.exports = {
     findById,
     addUser,
+    findBy,
 }
